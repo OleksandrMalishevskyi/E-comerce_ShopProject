6f82bd7b8cdf68528ea9a862517011cd445ecdfd
Sasza <sasza.vcn@gmail.com>
2022-11-01 19:57:36 +0100

    rectError16.3

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,20 @@
+import express from 'express'
+const router = express.Router()
+import {
+  addOrderItems,
+  // getOrderById,
+  // updateOrderToPaid,
+  // updateOrderToDelivered,
+  // getMyOrders,
+  // getOrders,
+} from '../controllers/orderController.js'
+import { protect } from '../middleware/authMiddleware.js'
+
+router.route('/').post(protect, addOrderItems)
+// router.route('/myorders').get( getMyOrders)
+// router.route('/:id').get( getOrderById)
+// router.route('/:id/pay').put( updateOrderToPaid)
+// router.route('/:id/deliver').put( updateOrderToDelivered)
+
+
+export default router
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,6 +12,7 @@ import {
     userDetailsReducer,
     userUpdateProfileReducer,
        } from './reducers/userReducers'
+import { orderCreateReducer } from './reducers/orderReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -21,28 +22,36 @@ const reducer = combineReducers({
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
-  
+    orderCreate: orderCreateReducer,
 })
 
-const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse
-(localStorage.getItem('cartItems')) : []
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : []
 
 const userInfoFromStorage = localStorage.getItem('userInfo')
   ? JSON.parse(localStorage.getItem('userInfo'))
   : null
 
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress')
+  ? JSON.parse(localStorage.getItem('shippingAddress'))
+  : {}
+
 const initialState = {
-    cart: {cartItems: cartItemsFromStorage},
-    userLogin: { userInfo: userInfoFromStorage },
+  cart: {
+    cartItems: cartItemsFromStorage,
+    shippingAddress: shippingAddressFromStorage,
+  },
+  userLogin: { userInfo: userInfoFromStorage },
 }
 
 const middleware = [thunk]
 
 const store = createStore(
-    reducer,
-    initialState,
-    composeWithDevTools(applyMiddleware(...middleware)
-    ))
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+)
 
 
 export default store
